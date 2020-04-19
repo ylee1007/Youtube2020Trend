e89b3878746c9bfb2bbc8009116360d854cc1e9d
younanna <anna981209@gmail.com>
2020-04-19 18:58:44 -0500

    MSNBC date

--- publishDate.py
+++ publishDate.py
@@ -8,32 +8,23 @@ import os
 # os.getcwd()
 # print(os.listdir(os.getcwd()))
 
-df = pd.read_excel(r'list_mode_export_fox.xlsx')
+df = pd.read_excel(r'list_mode_export_msnbc_final.xlsx')
+
 numDate = []
 
-for x in range(2800, 2880):
+for x in range(1700, 1701):
     url = df["Original Url"][x]
 
     content = requests.get(url)
     # create beautiful soup object to parse HTML
     soup = bs(content.content, "html.parser")
+    #print(soup)
     # date when the video was published
-    temp_publishDate = soup.find("strong", attrs={"class": "watch-time-text"}).text
+    # temp_publishDate = soup.find("strong", attrs={"class": "watch-time-text"}).text
+    temp_publishDate = soup.find("strong", attrs={"class": "watch-time-text"})
+    #print(temp_publishDate)
+    temp_publishDate = temp_publishDate.text
     publishDate = temp_publishDate[5:]
     numDate.append(publishDate)
 
 print(numDate)
-
-
-
-
-#url = df["Original Url"][0]
-# print(url)
-# download HTML code
-#content = requests.get(url)
-# create beautiful soup object to parse HTML
-#soup = bs(content.content, "html.parser")
-# date when the video was published
-#temp_publishDate = soup.find("strong", attrs={"class": "watch-time-text"}).text
-#publishDate = temp_publishDate[5:]
-#print(publishDate)
